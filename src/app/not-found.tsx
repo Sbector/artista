07f0e51eb33aafd0c33122b59716eec51eff6c47
Sbector <saulbector@gmail.com>
2024-07-12 16:01:40 -0600

    Notfound basic page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div>
+            <h2>Página no encontrada</h2>
+            <p>No se pudo encontrar la página solicitada</p>
+        </div>
+    )
+}
