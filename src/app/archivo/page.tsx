b93bd91bdc94cdda73bd78742ef3cf1a2cf42de3
Sbector <saulbector@gmail.com>
2024-07-12 16:02:02 -0600

    update to new button

--- src/app/archivo/page.tsx
+++ src/app/archivo/page.tsx
@@ -1,16 +1,11 @@
 import Button from "../components/Button"
-import Link from "next/link"
 
 export default function Archivo() {
     return (
         <div>
             <h1>archivo</h1>
-            <Link href="./archivo/obra">
-                <Button>obra</Button>
-            </Link>
-            <Link href="./archivo/proyectos">
-                <Button>proyectos</Button>
-            </Link>
+            <Button href="./archivo/obra">obra</Button>
+            <Button href="./archivo/proyectos">proyectos</Button>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,13 @@
 import Button from "./components/Button"
-import Link from "next/link"
-
 
 export default function Home() {
     return (
         <div>
             <h1>sbector</h1>
-            <Link href="./about">
-                <Button>acerca de</Button>
-            </Link>
-            <Link href="./archivo">
-                <Button>archivo</Button>
-            </Link>
-            <Link href="./blog">
-                <Button>blog</Button>
-            </Link>
-            <Link href="./contacto">
-                <Button>contacto</Button>
-            </Link>
+            <Button href="./about">about</Button>
+            <Button href="./archivo">archivo</Button>
+            <Button href="./blog">blog</Button>
+            <Button href="./contacto">contacto</Button>
         </div>
     )
 }
