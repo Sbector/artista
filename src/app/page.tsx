dcd04fa2b400c49774216727d87fab603eaa187d
Sbector <saulbector@gmail.com>
2024-07-12 22:53:37 -0600

    update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,7 @@ export default function Home() {
     return (
         <div>
             <h1>Bienvenide!</h1>
-            <h2>A continuación puedes navegar por las páginas de este sitio</h2>
-            <Button href="./about">about</Button>
-            <Button href="./archivo">archivo</Button>
-            <Button href="./blog">blog</Button>
-            <Button href="./contacto">contacto</Button>
+            <p>Este es el sitio de artista de sbector</p>
         </div>
     )
 }
