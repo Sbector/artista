e257c3c751b73dd01bbb7b15f60c313e80123971
Sbector <saulbector@gmail.com>
2024-07-14 23:53:59 -0600

    Avoid key problem at obras map and add back buttons

--- src/app/archivo/obra/[[...slug]]/page.tsx
+++ src/app/archivo/obra/[[...slug]]/page.tsx
@@ -3,38 +3,39 @@ import { Obras } from "../../../constants"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { Props } from "../../../types"
+import Button from "@/app/_components/Button"
 
 export default function Obra({ params, }: Props) {
     const pathname = usePathname()
+    const obra = {
+        title: "",
+        year: "",
+        description: ""
+    }
+
+    Obras.map(({ title, year, description }) => {
+
+        if (pathname.includes(title)) {
+            obra.title = title
+            obra.year = year
+            obra.description = description
+        }
+    })
 
     if (params.slug?.length === 1) {
-        Obras.map(({ title, year, description }) => {
-        })
 
         return (
             <div>
-                <Link href="/archivo/obra">volver a obras</Link>
-                {Obras.map(({ title, year, description }) => {
-                    console.log(pathname)
-                    if (pathname.includes(title)) {
-                        return (
-                            <>
-                                <h1>{title}</h1>
-                                <h2>{year}</h2>
-                                <p>{description}</p>
-                            </>
-                        )
-                    }
-
-                })
-                }
-
-
+                <Button href="/archivo/obra">volver a obras</Button>
+                <h1>{obra.title}</h1>
+                <h2>{obra.year}</h2>
+                <p>{obra.description}</p>
             </div>
         )
     } else {
         return (
             <div>
+                <Button href="/archivo">archivo</Button>
                 <h1>obras</h1>
                 <ul>
                     {Obras.map(({ title, year }) => (
