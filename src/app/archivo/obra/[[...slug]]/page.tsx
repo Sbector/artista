228c27745920a2fd24d1ddc4f5f83de1c686a887
Sbector <saulbector@gmail.com>
2024-07-15 00:46:30 -0600

    Revert github actions

--- src/app/archivo/obra/[[...slug]]/page.tsx
+++ src/app/archivo/obra/[[...slug]]/page.tsx
@@ -5,14 +5,6 @@ import { usePathname } from "next/navigation"
 import { Props } from "../../../types"
 import Button from "@/app/_components/Button"
 
-export async function generateStaticParams() {
-    const obra = await fetch('https://.../obra').then((res) => res.json())
-   
-    return obra.map((obra:any) => ({
-      slug: obra.slug,
-    }))
-  }
-
 export default function Obra({ params, }: Props) {
     const pathname = usePathname()
     const obra = {
