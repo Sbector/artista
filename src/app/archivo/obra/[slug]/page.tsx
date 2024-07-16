651a51960005cc580415637158c31468a9eeeb12
Sbector <saulbector@gmail.com>
2024-07-15 20:11:30 -0600

    add generateStaticParams and enable dynamic routes

--- src/app/archivo/obra/[slug]/page.tsx
+++ src/app/archivo/obra/[slug]/page.tsx
@@ -1,11 +1,35 @@
-export const generateStaticParams = async () => {
-    return[{slug: "paricutin"}]
+import Button from "@/app/_components/Button"
+import { Obras } from "@/app/constants"
+
+export async function generateStaticParams() {
+    const obras = await Obras
+
+    return obras.map((obra)=>({
+        slug: obra.slug,
+    }))
 }
 
 export default function ObraPage(props: any) {
-    return(
+    const slug = props.params.slug
+    const obra = {
+        title: "",
+        year: "",
+        description: "",
+        slug: "",
+    }
+    Obras.map(({ title, year, description, slug }) => {
+        if (props.params.slug.includes(slug)) {
+            obra.title = title
+            obra.year = year
+            obra.description = description
+        }
+    })
+    return (
         <>
-        Holi
+            <Button href="/archivo/obra">obra</Button>
+            <h1>{obra.title}, {obra.year}</h1>
+            <div className="h-[200px] w-[200px] bg-black"></div>
+            <p>{obra.description}</p>
         </>
     )
 }
