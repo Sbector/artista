173201cb28a71582d2319001e57581b2e2b56ffc
Sbector <saulbector@gmail.com>
2024-07-19 21:18:47 -0600

    test image

--- src/app/archivo/proyectos/page.tsx
+++ src/app/archivo/proyectos/page.tsx
@@ -1,11 +1,13 @@
 import Card from "@/app/_components/Card";
 import Wrapper from "@/app/_components/Wrapper";
+const localUrl  = "/obras/shuniata.gif"
+const ghpagesUrl  = "/obras/shuniata.gif"
 
 export default function Proyectos() {
     return (
         <div>
             <Wrapper>
-                <Card title="Example" year="XXX" imgsrc="/obras/shuniata.gif" imgalt="" description="Descripción de prueba"></Card>
+                <Card title="Example" year="XXX" imgsrc="./obras/shuniata.gif" imgalt="" description="Descripción de prueba"></Card>
             </Wrapper>
         </div>
     )
