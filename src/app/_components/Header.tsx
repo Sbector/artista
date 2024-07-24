ab2c0a27a4d0b19e71e7fa2d6e3efef78bd30c88
Sbector <saulbector@gmail.com>
2024-07-23 21:53:37 -0600

    Nav update

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -4,6 +4,8 @@ import Nav from "./Nav"
 export default function Header() {
     return (
         <header className="
+        sticky
+        z-40
         top-0 
         w-screen
         md:w-[700px] md:mx-auto
