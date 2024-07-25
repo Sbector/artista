1caa845f1bbd8342327fe7d6c15a1ea95b0bee38
Sbector <saulbector@gmail.com>
2024-07-25 09:17:16 -0600

    add chalchiuh on constants

--- src/app/constants.tsx
+++ src/app/constants.tsx
@@ -3,10 +3,18 @@ import { Section } from "./types"
 import { Proyecto } from "./types"
 
 
-export const Obras: Obra [] = [
-   
+export const Obras: Obra[] = [
     {
-        title:"parícutin",
+        title: "chalchiuhtlicue",
+        year: "2024",
+        description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Efficitur mi libero taciti tortor etiam luctus auctor luctus conubia. Nunc efficitur mauris nostra nulla tempor fringilla elementum. Magna ultrices eleifend ultrices nulla cras ultricies. Ut sapien finibus sapien, sagittis turpis lectus magnis potenti. Aliquam inceptos fusce posuere facilisis ultricies augue. Curabitur accumsan dignissim netus, elit consectetur nostra ultricies ultrices. Mattis etiam dictum mauris eu donec platea aenean sapien. Nam litora ligula facilisi nisi pretium fusce consequat mus sollicitudin.",
+        slug: "chalchiuhtlicue",
+        imgsrc: "../../obras/paricutin.jpg",
+        imgalt: "Captura de pantalla de la experiencia virtual 3d chalchiuhtlicue",
+        iframesrc: "https://sbector.com/chalchiuhtlicue/"
+    },
+    {
+        title: "parícutin",
         year: "2023",
         description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Efficitur mi libero taciti tortor etiam luctus auctor luctus conubia. Nunc efficitur mauris nostra nulla tempor fringilla elementum. Magna ultrices eleifend ultrices nulla cras ultricies. Ut sapien finibus sapien, sagittis turpis lectus magnis potenti. Aliquam inceptos fusce posuere facilisis ultricies augue. Curabitur accumsan dignissim netus, elit consectetur nostra ultricies ultrices. Mattis etiam dictum mauris eu donec platea aenean sapien. Nam litora ligula facilisi nisi pretium fusce consequat mus sollicitudin.",
         slug: "paricutin",
@@ -15,7 +23,7 @@ export const Obras: Obra [] = [
         iframesrc: "https://sbector.com/paricutin/"
     },
     {
-        title:"subtle-sync",
+        title: "subtle-sync",
         year: "2023",
         description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Efficitur mi libero taciti tortor etiam luctus auctor luctus conubia. Nunc efficitur mauris nostra nulla tempor fringilla elementum. Magna ultrices eleifend ultrices nulla cras ultricies. Ut sapien finibus sapien, sagittis turpis lectus magnis potenti. Aliquam inceptos fusce posuere facilisis ultricies augue. Curabitur accumsan dignissim netus, elit consectetur nostra ultricies ultrices. Mattis etiam dictum mauris eu donec platea aenean sapien. Nam litora ligula facilisi nisi pretium fusce consequat mus sollicitudin.",
         slug: "subtle",
@@ -23,10 +31,10 @@ export const Obras: Obra [] = [
         imgalt: "Captura de pantalla de la experiencia virtual 3d subtle syn",
         iframesrc: "https://sbector.com/subtle-sync/"
     },
-    
+
 ]
 
-export const Sections: Section [] = [
+export const Sections: Section[] = [
     {
         label: 'about',
         route: '/about'
@@ -42,7 +50,7 @@ export const Sections: Section [] = [
     },
 ]
 
-export const Proyectos: Proyecto [] = [
+export const Proyectos: Proyecto[] = [
     {
         title: "shuniata",
         year: "2012",
